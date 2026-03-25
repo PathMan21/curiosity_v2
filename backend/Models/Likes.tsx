2396eb6e013fd27c83f00bab7aa25b8504cb1d79
Manon <145130728+PathMan21@users.noreply.github.com>
2026-03-26 00:25:53 +0100

    Ajouts de likes - travail facultatif pour à côté

---
+++ backend/Models/Likes.tsx
@@ -0,0 +1,28 @@
+import { Model, DataTypes, Optional } from 'sequelize'
+import bcrypt from 'bcrypt'
+import sequelizeDb from '../Config/dbInit'
+
+const Likes = sequelizeDb.define(
+  'Likes',
+  {
+    id: {
+      field: 'id',
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    userId: {
+        field: 'userId',
+        type: DataTypes.INTEGER,
+
+    },
+    contentId: {
+        field: 'contentId',
+        type: DataTypes.INTEGER,
+        
+    },
+    contentType: {
+        field: 'contentType',
+        type: DataTypes.STRING 
+    }
+  });
