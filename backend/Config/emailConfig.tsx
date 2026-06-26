17f300ffc61d892cd336b251056eb44498f3ab37
Manon <manon.lafosse1@gmail.com>
2026-06-26 17:58:11 +0200

    feat: email

--- backend/Config/emailConfig.tsx
+++ backend/Config/emailConfig.tsx
@@ -4,6 +4,7 @@ import "../Helpers/configLink";
 export const transport = nodemailer.createTransport({
     host: "smtp.scaleway.com",
     port: 587,
+    secure: false,
     auth: {
     user: process.env.AUTH_MAIL,
     pass: process.env.AUTH_PWD,
