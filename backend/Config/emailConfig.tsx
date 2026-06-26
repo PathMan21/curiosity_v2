e69e371a25ec761c59910310082fdec9878e4b61
Manon <manon.lafosse1@gmail.com>
2026-06-26 16:59:24 +0200

    feat: vps mail

--- backend/Config/emailConfig.tsx
+++ backend/Config/emailConfig.tsx
@@ -2,8 +2,9 @@ import nodemailer from 'nodemailer'
 
 import "../Helpers/configLink";
 export const transport = nodemailer.createTransport({
-  service: 'gmail',
-  auth: {
+    host: "smtp.scaleway.com",
+    port: 587,
+    auth: {
     user: process.env.AUTH_MAIL,
     pass: process.env.AUTH_PWD,
   },
