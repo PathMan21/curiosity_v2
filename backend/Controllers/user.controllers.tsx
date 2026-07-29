1b25b6f9bbdff715ee4f83aa59dc18dd7700de17
Manon <manon.lafosse1@gmail.com>
2026-07-29 17:15:43 +0200

    feature/bug-001

--- backend/Controllers/user.controllers.tsx
+++ backend/Controllers/user.controllers.tsx
@@ -9,7 +9,7 @@ const generateTokens = (userId: number) => ({
     expiresIn: '15m',
   }),
   refreshToken: jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: '7d',
+    expiresIn: '60s',
   }),
 })
 
