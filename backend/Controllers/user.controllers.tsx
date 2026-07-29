a38c44136c84420dd8389d8321fc3559911577ca
Manon <manon.lafosse1@gmail.com>
2026-07-29 17:18:19 +0200

    fix/erreur-validation-access-token

--- backend/Controllers/user.controllers.tsx
+++ backend/Controllers/user.controllers.tsx
@@ -9,7 +9,7 @@ const generateTokens = (userId: number) => ({
     expiresIn: '15m',
   }),
   refreshToken: jwt.sign({ userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: '60s',
+    expiresIn: '15m',
   }),
 })
 
